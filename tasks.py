b7ac2a44eef819a55d2a35081151c498a80edb52
Thomasstycke <s214742@student.dtu.dk>
2025-01-06 16:01:26 +0100

    fff

--- tasks.py
+++ tasks.py
@@ -73,3 +73,9 @@ def greet(ctx, name="World"):
 def python(ctx):
     """ """
     ctx.run("which python" if os.name != "nt" else "where python")
+
+@task
+def git(ctx, message):
+    ctx.run(f"git add .")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
